ddc55a4404a8525767d8c9e09c364969b373a8d9
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2025-11-26 22:14:23 +0530

    Set document title to movie title on page load

--- app/movies/[movieid]/page.js
+++ app/movies/[movieid]/page.js
@@ -212,7 +212,7 @@ export default function Page() {
 
         }
     }
-
+    document.title=show.title;
     if (loading) {
         return (
             <div className='w-[80%] mx-auto md:mt-32 max-md:mt-16'>
