93a578a252f02836603f856e0e538882a0d622e4
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2025-11-28 20:55:00 +0530

    Correct document title assignment logic
    Fix document title assignment after setting show data.

--- app/movies/[movieid]/page.js
+++ app/movies/[movieid]/page.js
@@ -39,7 +39,7 @@ export default function Page() {
                 }
 
                 setshow(data)
-                document.title=show.title
+                
 
 
                 // Find trailer
